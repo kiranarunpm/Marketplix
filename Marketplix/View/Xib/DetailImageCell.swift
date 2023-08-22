8b63444b3989865c024775a2a840ebf3fe01a0c7
Kiran <Kiran.pm@innovature.com>
2023-08-22 16:05:04 +0530

    Added Product detail page

---
+++ Marketplix/View/Xib/DetailImageCell.swift
@@ -0,0 +1,17 @@
+//
+//  DetailImageCell.swift
+//  Marketplix
+//
+//  Created by Kiran P M on 22/08/23.
+//
+
+import UIKit
+
+class DetailImageCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Marketplix/ViewModel/ListingVM.swift
@@ -0,0 +1,8 @@
+//
+//  ListingVM.swift
+//  Marketplix
+//
+//  Created by Kiran P M on 22/08/23.
+//
+
+import Foundation
